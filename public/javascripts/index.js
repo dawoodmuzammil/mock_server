0c553f62eee6bc27fb2505609dc29609d8084f79
Dawood Muzammil <dawood.malik@bordatech.com>
2021-03-27 02:53:16 +0300

    Dynamic fetch of .yaml based on product. Get path data

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,18 +10,29 @@ module.exports = {
 			let api = await SwaggerParser.dereference(path);
 			return api;
 		} catch (e) {
-			res.send(e);
+			return e;
 		}
 	},
 
 	// splits the url by '/' and extracts info
-	getUrlPaths(req) {
+	getInfoFromUrl(req) {
 		var paths = req['url'].split('/');
 		return {
 			host: req.get('host'),
 			organization: paths[2],
 			product: paths[3],
 			api_path: `/${paths[4]}/${paths[5]}`,
+			method: req.method.toLowerCase()
 		};
+	},
+
+	// gets data related to the path from yaml file
+	// returns 404 Not Found when provided path does not exist in .yaml
+	getPathData(api, url, res) {
+		try {
+			return api['paths'][url.api_path][url.method]; // get all possible responses for that path
+		} catch (error) {
+			res.status(404).send(`The path ${url.api_path} does not exist`);
+		}
 	}
 }
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,22 @@ var express = require('express');
 var router = express.Router();
 var underscore = require("underscore");
 
-var { getDereferencedYAML, getUrlPaths } = require("../public/javascripts/index");
+var { getDereferencedYAML, getInfoFromUrl, getPathData } = require("../public/javascripts/index");
 
 router.all('/*', async function (req, res) {
-	try {
-		let api = await getDereferencedYAML('./test.yaml');
-		var urlPaths = getUrlPaths(req);
-		var responses = api["paths"][req.url][req.method.toLowerCase()]["responses"]; // get all possible responses for that path
-		const [responseCode, responseBody] = Object.entries(responses)[0];
-		const schema = responseBody.content["application/json"].schema;
-		// res.send(schema);
-		// fakeProperties(schema);
-		// queryParameters(schema, {});
-		// console.log('api===', responses)
-		// faker.jsf.resolve(schema).then(mockData => res.status(200).send({ code: responseCode, body: mockData })).catch(err => res.status(500).send(err));
-	} catch (error) {
-		res.send(error);
-	}
+	var url = getInfoFromUrl(req);
+
+	var api = await getDereferencedYAML(`./products/${url.product}/test.yaml`);
+
+	var pathData = getPathData(api, url, res);
+
+	const [responseCode, responseBody] = Object.entries(pathData)[0];
+	const schema = responseBody.content["application/json"].schema;
+	// res.send(schema);
+	// fakeProperties(schema);
+	// queryParameters(schema, {});
+	// console.log('api===', responses)
+	// faker.jsf.resolve(schema).then(mockData => res.status(200).send({ code: responseCode, body: mockData })).catch(err => res.status(500).send(err));
 });
 
 module.exports = router;
